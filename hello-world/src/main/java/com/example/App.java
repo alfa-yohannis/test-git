1413902f46dd39aea36b94c0df431c395a49aa14
alfa-ryano <alfa.ryano@live.com>
2024-09-27 07:52:01 +0700

    add argument

--- hello-world/src/main/java/com/example/App.java
+++ hello-world/src/main/java/com/example/App.java
@@ -4,11 +4,12 @@ package com.example;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println("BBBBBBBBb Hello World!");
-        System.out.println( "AAAAAAAAAAAa Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        if (args.length > 0) {
+            System.out.println("Hello, " + args[0]);
+        } else {
+            System.out.println("Hello World!");
+        }
     }
 }
